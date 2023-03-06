5723ecda1ef0216276e99906700f86f46310f61e
fenya <dim74.09@mail.ru>
2023-03-06 11:29:38 +0500

    Просто чтобы было

--- Student.cpp
+++ Student.cpp
@@ -1,1 +1,54 @@
 #include "StudentDB.h"
+#include <sstream>
+#include <string>
+
+Student::Student(std::string name, std::string surname, std::string patronymic, int groupID)
+{
+	if(name.size() > NAME_SIZE)
+		throw "too long name.";
+	if(surname.size() > SURNAME_SIZE)
+		throw "too long surname.";
+	if(name.size() > PATRONYMIC_SIZE)
+		throw "too long patronymic.";
+	_name = name;
+	_surname = surname;
+	_patronymic = patronymic;
+	_groupID = groupID;
+}
+
+Student::Student(std::string tblRecord)
+{
+	std::string field;
+	std::stringstream ss(tblRecord);
+	std::getline(ss, field, ':');
+	std::getline(ss, _name, ':');
+	std::getline(ss, _surname, ':');
+	std::getline(ss, _patronymic, ':');
+	std::getline(ss, field, ':');
+	_groupID = std::stoi(field);
+}
+
+std::string Student::name()
+{
+	return _name;
+}
+
+std::string Student::surname()
+{
+	return _surname;
+}
+
+std::string Student::patronymic()
+{
+	return _patronymic;
+}
+
+int Student::groupID()
+{
+	return _groupID;
+}
+
+std::string Student::toString()
+{
+	return _name + ":" + _surname + ":" + _patronymic + ":" + std::to_string(_groupID);
+}
--- StudentDB.h
+++ StudentDB.h
@@ -1,26 +1,40 @@
 #include <string>
+#include <vector>
 
 class Student
 {
 	public:
+		static const int NAME_SIZE = 20;
+		static const int SURNAME_SIZE = 20;
+		static const int PATRONYMIC_SIZE = 20;
+		static const int RECORD_FIELDS_QUANT = 5;
 		Student();
+		std::string name();
+		std::string surname();
+		std::string patronymic();
+		int groupID();
 		Student(std::string, std::string, std::string, int);
+		Student(std::string);
 		std::string toString();
 	private:
-		std::string name;
-		std::string surname;
-		std::string patronymic;
-		int group_id;
+		std::string _name;
+		std::string _surname;
+		std::string _patronymic;
+		int _groupID;
 };
 
 class StudentsTable
 {
 	public:
-		StudentTable(std::string);
+		StudentsTable(std::string);
+		int getstQnt();
+		void create();
+		void open();
 		void add(Student);
-		Student* get(int);
-		Student* getall();
+		std::vector<Student> getAll();
+		// bool isValid_***;
 	private:
-		int curID;
-		int numOfStudents;
+		int _curID;
+		int _stQnt;
+		std::string _path;
 };
